69fb01efd97bbe7f12b0edf90665355f5b1bac53
Maria <j.maria.j@gmail.com>
2018-05-12 22:09:51 -0500

    Added files

---
+++ server.js
@@ -0,0 +1,32 @@
+/* INclude the static file webserver library */
+var static = require('node-static');
+
+/* Include the http server library */
+var http = require('http');
+
+/*Assume that we are running on Heroku */
+var port = process.env.PORT;
+var directory = __dirnmae = '/public';
+
+/* If we aren't on Heroku, then we need to readjust the port and directory info and we know that bc port won't be set */
+if(typeof port == 'undefined' || !port) {
+    directory = './public';
+    port = 8080;
+}
+
+/* Set up a static web-serve that will deliver files from the filesystem */
+var file = new static.Server(directory);
+
+/* construct an http server that gets files from the file server*/
+var app = http.createServer(
+    function(request,response){
+        request.addListener('end',
+            function(){
+                file.serve(request,response);
+            }
+    ).resume();
+    }
+).listen(port);
+
+console.log('The server is running');
+
